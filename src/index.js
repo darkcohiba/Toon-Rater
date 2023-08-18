987737ddfda96052f7daca97792cad99469065dc
Sam Waters <samuel.waters@gmail.com>
2023-08-18 14:25:46 -0600

    chore: added fetch for backend data

--- src/index.js
+++ src/index.js
@@ -170,7 +170,7 @@ function filterToonsByNetwork(network) {
 // fetch data from render json server
 function renderToonsOnRender(){
   console.log("inside render toons on render function")
-  fetch("http://toon-site.onrender.com:3000/toons")
+  fetch("https://toon-site.onrender.com:3000/toons")
   .then(response => response.json())
   .then(data => {
     console.log("inside render fetch")
